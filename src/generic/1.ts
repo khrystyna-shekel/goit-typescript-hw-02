4c8815469ff46141e3c8f7f150e72725c001d45d
khrystyna-shekel <christina.shekel@gmail.com>
2024-01-06 20:58:24 +0200

    done

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,14 +3,15 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise () {
+// Promise<(string | number)[]>;
+
+function getPromise(): Promise<Array<string | number>> {
   return new Promise((resolve) => {
-    resolve(['Text', 50]);
+    resolve(["Text", 50]);
   });
 }
 
-getPromise()
-.then((data) => {
+getPromise().then((data) => {
   console.log(data);
 });
 
--- src/generic/7.ts
+++ src/generic/7.ts
@@ -4,16 +4,19 @@
 */
 
 export enum UserRole {
-  admin = 'admin',
-  editor = 'editor',
-  guest = 'guest',
+  admin = "admin",
+  editor = "editor",
+  guest = "guest",
 }
 
 // Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
-  admin: 'Admin User',
-  editor: 'Editor User',
-  guest: 'Guest User',
+
+type UserRoleDescription = Record<UserRole, string>;
+
+const RoleDescription: UserRoleDescription = {
+  admin: "Admin User",
+  editor: "Editor User",
+  guest: "Guest User",
 };
 
 export {};
